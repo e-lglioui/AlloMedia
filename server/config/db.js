45513f50f3208397863b5051905dfb65af6ef914
e-lglioui <elgliouiyoucode@gmail.com>
2024-10-01 16:53:55 +0200

    regroper backend dans folder server

--- server/config/db.js
+++ server/config/db.js
@@ -5,7 +5,7 @@ dotenv.config();
 
 const connectDB = async () => {
     try {
-        await mongoose.connect(process.env.MONGO_URI);
+        await mongoose.connect(process.env.DB_URI);
         // await mongoose.createConnection(process.env.MONGO_URI, { useNewUrlParser: true });
         console.log('MongoDB connected');
     } catch (error) {
